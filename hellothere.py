80b67d18d02f7e1b6424afcfb1782aa2ad076391
vvamzy <vvamzy@gmail.com>
2021-09-10 15:23:47 +0530

    Initial commit

---
+++ hellothere.py
@@ -0,0 +1,15 @@
+#########################TE$TCOD3#####################
+
+user_name = str(input("Enter your name: "))
+print("")
+user_id = input("Enter your ID number: ")
+print("")
+user_team = str(input("Enter your Team name: "))
+print("")
+user_project = str(input("Enter project name: "))
+print("")
+user_message = str(input("Message: "))
+print("")
+message_txt = print(f" Your message: {user_message}\n")
+
+summary_txt = print(f" Hello there, {user_name} thank you for logging in with your ID:{user_id}. Your message to your team {user_team} and TL of project {user_project} has been sent! Thank you.")
